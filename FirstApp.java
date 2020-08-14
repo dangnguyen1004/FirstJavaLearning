fb2373558e527a7075195feb1e8315b9672c1f99
dangnguyen1004 <nguyendangit04@gmail.com>
2020-08-14 23:45:57 +0700

    Update

--- FirstApp.java
+++ FirstApp.java
@@ -1,5 +1,6 @@
 public class FirstApp{
     public static void main(String[] args){
-        System.out.print("Hello World");
+        System.out.print("Hello World\n");
+        System.out.print("Doi khong nhu la mo\n");
     }
 }
